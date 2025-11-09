56e89de706fbf46170a36dff7febedcf6e3918a0
tempwalia <temp.waliat2@gmail.com>
2025-11-09 18:35:47 +0530

    Pakcages Installed

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+setup(
+    name = "chatbot",
+    version ="0.1.0",
+    author ="Tikam Singh",
+    packages = find_packages(),
+    install_requires =[]
+)
