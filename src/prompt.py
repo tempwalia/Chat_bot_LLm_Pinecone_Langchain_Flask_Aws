db027e7446e6965ecd3a721ceae78ba61b48d7b4
tempwalia <temp.waliat2@gmail.com>
2025-11-11 11:31:03 +0530

    geography of india as data source

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,7 @@
 
 
 system_prompt = (
-    "You are an Medical assistant for question-answering tasks. "
+    "You are an  assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
